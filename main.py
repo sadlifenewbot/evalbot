e5057b3ee5f426137d659dc6418791cf61ce94d1
sadlifenewbot <89448156+sadlifenewbot@users.noreply.github.com>
2021-08-24 17:06:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import discord
 from discord.ext import commands
 
+from aioconsole import aexec
 
+import sys
+import io
+
+out, err = io.StringIO(), io.StringIO()
+sys.stdout = out
+sys.stderr = err
+
+previous_result = "The bot is ready!"
 activity = discord.Activity(type=discord.ActivityType.listening, name="eval")
-
 bot = commands.Bot(command_prefix=['!'], activity=activity)
 
+@bot.event
+async def on_ready():
+    print("The bot is ready!")
+
+
 @bot.command(name="eval")
-async def eval(ctx,  *,python_code):
+async def eval(ctx,  *,python_code, previous_result=previous_result):
     # """for info do 'sudo help invite'"""
     python_code = python_code.replace("```py", "")
     python_code = python_code.replace("```", "")
-    embed = discord.Embed(title="Result", description=f"```py{eval(python_code)}\n```")
+    out, err = io.StringIO(), io.StringIO()
+    sys.stdout = out
+    sys.stderr = err
+
+    await aexec(python_code)
+    results = out.getvalue()
+    errors = err.getvalue()
+    embed = discord.Embed(title="Result", description=f"```py\n{results}\n```")
     await ctx.send(embed=embed)
+    previous_result = results
 
 token = "ODc5NjMyNDk1OTk2NDUyODc0.YSSjmQ.G5CfjCEMzFxbDq8BYjHnjNvM7"
 bot.run(token + "kk")
