0fcf3c33cc52550c8a781c858dde1c2bce2f05ab
sadlifenewbot <89448156+sadlifenewbot@users.noreply.github.com>
2021-08-24 14:59:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def eval(ctx,  *,python_code):
     # """for info do 'sudo help invite'"""
     python_code = python_code.replace("```py", "")
     python_code = python_code.replace("```", "")
-    embed = discord.Embed(title="Result", description=f"```py{eval(python_code)}\n```", color=#FFFFFF)
+    embed = discord.Embed(title="Result", description=f"```py{eval(python_code)}\n```", color=FFFFFF)
     await ctx.send(embed=embed)
 
 token = "ODc5NjMyNDk1OTk2NDUyODc0.YSSjmQ.G5CfjCEMzFxbDq8BYjHnjNvM7"
