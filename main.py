26ba09a66f2819c42264c1737235ac2723f01f4f
sadlifenewbot <89448156+sadlifenewbot@users.noreply.github.com>
2021-08-24 14:50:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+activity = discord.Activity(type=discord.ActivityType.listening, name="eval")
+
+bot = commands.Bot(command_prefix=['!'], intents=intents, activity=activity)
+
+@bot.command(name="eval", ignore_extra)
+async def eval(ctx,  *,python_code):
+    # """for info do 'sudo help invite'"""
+    python_code = python_code.replace("```py", "")
+    python_code = python_code.replace("```", "")
+    embed = discord.Embed(title="Result", f"```py{eval(python_code)}\n```")
+    await ctx.send(embed=embed)
+
+token = "ODc5NjMyNDk1OTk2NDUyODc0.YSSjmQ.G5CfjCEMzFxbDq8BYjHnjNvM7"
+bot.run(token + "kk")
